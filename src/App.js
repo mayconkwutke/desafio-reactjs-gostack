7170c1b60f4d157675fd73454c6fb9ab60509dc3
Maycon Kester Wutke <mayconkester@gmail.com>
2020-12-16 07:51:47 -0400

    Commit 1: Desafio ReactJS GoStack

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import api from "./services/api"
+
+import "./styles.css";
+
+
+
+function App() {
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepository(response.data);
+    })
+  }, []);
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: "Pojeto com NodeJS",
+      techs: "Maycon Kester Wutke",
+      likes: 0
+    });
+
+    const repository = response.data;
+
+    setRepository([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+    
+    const newRepositories = repositories.filter(
+      repository => repository.id !== id
+    );
+    setRepository(newRepositories);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => <li key={repository.id}>{repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+        </li>)}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
